d36979c79944fc2623b71adc18d82c81646b17b1
md <maciek.dudek@atkins.cloud.dk>
2024-03-14 09:37:24 +0000

    New logic for Query Template creation

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,38 @@ pattern <- "^https?://[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,}(\\:[0-9]+)?(/\\S*)?$"
 isValidUrl = function(url) {
   grepl(pattern, url)
 }
+
+extractTemplateParamStringDefaultValue = function(meta) {
+  value <- NULL
+
+  if(exists('default', where = meta)) {
+    value <- meta$default
+  }
+
+  if(exists('oneOf', where = meta) && length(meta$oneOf) > 0) {
+    value <- meta$oneOf[[1]]$const
+  }
+
+  if(is.null(value) || nchar(value) == 0) NULL else value
+}
+
+extractTemplateParamArrayDefaultValue = function(meta) {
+
+  if(exists('items', where = meta)) {
+
+    if (exists('oneOf', meta$items) && length(meta$items$oneOf) > 0) {
+      value <- meta$items$oneOf[[1]]$const
+      return(list(value))
+    }
+
+  }
+
+  NULL
+}
+
+
+extractTemplateParamDefaultValue = function(meta) {
+  switch (meta$type,
+    "string" = extractTemplateParamStringDefaultValue(meta),
+    "array"  = extractTemplateParamArrayDefaultValue(meta))
+}
